a291e109da764d5304bc4c7dccdb5d42689c1851
fnarit <naritsararat@gmail.com>
2020-04-12 10:58:24 +0700

    Background

---
+++ Background.cpp
@@ -0,0 +1,52 @@
+#include"func.h"
+using namespace sf;
+
+int main()
+{
+    //set render window
+    float w_height=700, w_width=500;
+    RenderWindow window(VideoMode(w_width, w_height), "English Easy",Style::Close);
+
+    Texture texture;
+    if (!texture.loadFromFile("C://Users/USER/Desktop/ProjectPracEng/pic/3choices.png"));
+    Sprite background(texture);
+
+
+    //create button and set position
+    Bton A(350,70,w_width/2,275);
+    Bton B(350,70,w_width/2,400);
+    Bton C(350,70,w_width/2,525);
+    RectangleShape btonA(Vector2f(A.Lx,A.Ly));
+    btonA.setPosition(A.posx,A.posy);
+    btonA.setOrigin(A.Lx/2,A.Ly/2);
+    RectangleShape btonB(Vector2f(B.Lx,B.Ly));
+    btonB.setPosition(B.posx,B.posy);
+    btonB.setOrigin(B.Lx/2,B.Ly/2);
+    RectangleShape btonC(Vector2f(C.Lx,C.Ly));
+    btonC.setPosition(C.posx,C.posy);
+    btonC.setOrigin(C.Lx/2,C.Ly/2);
+
+
+    while (window.isOpen())
+    {
+        Event evnt;
+        while (window.pollEvent(evnt))
+        {
+            if (evnt.type == Event::Closed)
+                window.close();
+
+        }
+
+        window.clear();
+        window.draw(background);
+        window.draw(btonA);
+        window.draw(btonB);
+        window.draw(btonC);
+        window.display();
+
+    }
+
+    return 0;
+
+}
+
