1df6b5eed1d4892a7f1542b6d85e4b5d318ac0e0
sorrybro55555 <zom_bie1596@hotmail.com>
2020-04-13 10:03:01 +0700

    GameOver

--- main.cpp
+++ main.cpp
@@ -14,49 +14,44 @@ int main()
     if (!font.loadFromFile("Fonts/Metropolian-Display.ttf"))
         throw("CLOUD NOT LOAD FONT!");
 
+    string Yscore = "YOUR SCORE : ";
+    string Hscore = "HIGH SCORE : ";
     Text text1;
     Text text2;
     Text text3;
     Text text4;
-    Text text5;
 
     text1.setFont(font);
     text1.setCharacterSize(20);
     text1.setColor(Color::Red);
     text1.setStyle(Text::Bold);
-    text1.setString("TIME");
+    text1.setString("May the force be with you.");
     text1.setOrigin(text1.getGlobalBounds().width /2,text1.getGlobalBounds().height /2);
-    textTime.setPosition(70,660);
+    text1.setPosition(window.getSize().x/2,window.getSize().y/3+250);
 
-    text2.setString("RandomWord");
-    text2.setOrigin(textC1.getGlobalBounds().width /2,textC1.getGlobalBounds().height /2);
-    text2.setPosition(Choice1.posx,Choice1.posy-5);
-    text2.setString("RandomWord");
-    text2.setOrigin(textC2.getGlobalBounds().width /2,textC2.getGlobalBounds().height /2);
-    text2.setPosition(Choice2.posx,Choice2.posy-5);
+    text2.setFont(font);
+    text2.setCharacterSize(50);
+    text2.setColor(Color::Red);
+    text2.setStyle(Text::Bold);
+    text2.setString("GAME OVER");
+    text2.setOrigin(text2.getGlobalBounds().width /2,text2.getGlobalBounds().height /2);
+    text2.setPosition(window.getSize().x/2,window.getSize().y/4+30);
 
-    text3.setString("RandomWord");
-    text3.setOrigin(textC1.getGlobalBounds().width /2,textC1.getGlobalBounds().height /2);
-    text3.setPosition(Choice1.posx,Choice1.posy-5);
-    text3.setString(RandomWord");
-    text3.setOrigin(textC2.getGlobalBounds().width /2,textC2.getGlobalBounds().height /2);
-    text3.setPosition(Choice2.posx,Choice2.posy-5);
+    text3.setFont(font);
+    text3.setCharacterSize(20);
+    text3.setColor(Color::Red);
+    text3.setStyle(Text::Bold);
+    text3.setString(Hscore);
+    text3.setOrigin(textHS.getGlobalBounds().width /2,textHS.getGlobalBounds().height /2);
+    text3.setPosition(window.getSize().x/2,window.getSize().y/2-42);
 
     text4.setFont(font);
     text4.setCharacterSize(20);
     text4.setColor(Color::Red);
     text4.setStyle(Text::Bold);
-    text4.setString("                                          ");
-    text4.setOrigin(text3.getGlobalBounds().width /2,text3.getGlobalBounds().height /2);
-    text4.setPosition(window.getSize().x/2,window.getSize().y/2+20);
-
-    text5.setFont(font);
-    text5.setCharacterSize(20);
-    text5.setColor(Color::Red);
-    text5.setStyle(Text::Bold);
-    text5.setString("BACK");
-    text5.setOrigin(text4.getGlobalBounds().width /2,text4.getGlobalBounds().height /2);
-    text5.setPosition(80, 50);
+    text4.setString(Yscore);
+    text4.setOrigin(text4.getGlobalBounds().width /2,text4.getGlobalBounds().height /2);
+    text4.setPosition(window.getSize().x/2,window.getSize().y/2+42);
 
 
 
@@ -76,7 +71,6 @@ int main()
         window.draw(text2);
         window.draw(text3);
         window.draw(text4);
-        window.draw(text5);
         window.display();
 
     }
